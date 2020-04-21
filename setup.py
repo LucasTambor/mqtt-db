cd66d7a67960e3dfa77a3b9c5cdf2dabc5dde346
Lucas Tamborrino <lucastamborrino@gmail.com>
2020-04-21 17:16:52 -0300

    Add setuptools suport

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+import sys
+from setuptools import setup
+setup(
+    name = "mqtt-db",
+    version = "1.0.0",
+    author="Lucas Tamborrino",
+    author_email = "lucastambor92@gmail.com",
+    description = "Mqtt and Sqlite3",
+    license = "MIT",
+)
